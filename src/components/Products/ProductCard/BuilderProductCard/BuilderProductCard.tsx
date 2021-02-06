14176f283cb5e6fd10d5b681561ed7f7a8a911b2
Valentin <valentinspasov9669@gmail.com>
2021-02-07 01:15:27 +0200

    [UPDATE] responsive checkout page

--- src/components/Products/ProductCard/BuilderProductCard/BuilderProductCard.tsx
+++ src/components/Products/ProductCard/BuilderProductCard/BuilderProductCard.tsx
@@ -1,6 +1,5 @@
-import React, { Component, FunctionComponent, useCallback, useEffect, useState } from 'react'
-import { GET_FULL_COMPONENT_URL, GET_FULL_IMAGE_URL } from '../../../../constants'
-import { useIsMounted } from '../../../../hooks/useIsMounted'
+import React, { FunctionComponent } from 'react'
+import { GET_FULL_IMAGE_URL } from '../../../../constants'
 import { Product } from '../../../../types/product/Product'
 import styles from './BuilderProductCard.module.scss'
 
@@ -9,28 +8,8 @@ interface Props {
 }
 
 export const BuilderProductCard: FunctionComponent<Props> = props => {
-    const [component, setComponent] = useState<Component | null>(null)
-
-    const isMounted: React.MutableRefObject<boolean> = useIsMounted()
-
-    const fetchData = useCallback(async () => {
-        const response = await fetch(
-            GET_FULL_COMPONENT_URL(props.product.type, String(props.product.id))
-        )
-
-        const data = await response.json()
-
-        if (!isMounted.current) return
-
-        if (!response.ok) {
-            setComponent(null)
-            return
-        }
-        setComponent(data.component)
-    }, [props, isMounted])
-
     return (
-        <button className={styles.root} onClick={fetchData}>
+        <button className={styles.root}>
             <img src={GET_FULL_IMAGE_URL(props.product.images[0].url)} alt={props.product.name} />
             <div>
                 <p>{props.product.name}</p>
